b1374d8d4f02fc10e7bf1f534494cec7a708bc64
Viktoria Draenkova <vikadraenkova@yandex.by>
2023-07-30 15:21:29 +0300

    Программа для практической работы 2 попытка(с дополнениями)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -37,6 +37,10 @@ class Calculator {
 
 class Formatter{
     public static String format(double sum, int  amount){
+        int numb= (int) ((sum/amount)%100);
+        if(numb>=11&&numb<=19){
+            return "рублей";
+        }
         int digit= (int) ((sum/amount)%10);
         switch (digit){
             case 1:
